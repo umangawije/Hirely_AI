a7b5b6eed00005ad216c9d10988a1b4cd9744a5a
umangawije <127008042+umangawije@users.noreply.github.com>
2024-05-24 15:53:54 +0530

    create folder structure

--- aidf-front-end/src/main.tsx
+++ aidf-front-end/src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+
+import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    
+    
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
--- aidf-front-end/src/umanga.tsx
+++
@@ -1,5 +0,0 @@
-function Umanga(){
-    return <h1>Umanga</h1>
-}
-
-export default Umanga;
